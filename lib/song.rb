a301ce2e882c4157d1bad8c6f05887f08f1a7fb8
Melissa Real <melissa.real@hotmail.com>
2018-04-30 11:51:15 +0000

    automatically backed up by learn

---
+++ lib/song.rb
@@ -0,0 +1,34 @@
+class Song
+  attr_accessor :name, :artist, :genre
+
+  @@count = 0
+  @@genres = []
+  @@artists = []
+  @@genres = []
+
+  def initialize(name, artist, genre)
+    @@count +=1
+    @@artists << artist
+    @@genres << genre
+  end
+
+  def self.count
+    @@count
+  end
+
+  def artists
+    @@artists == artist.uniq!
+    @@artists
+  end
+
+  def genres
+    @@genres == @@genres.uniq!
+    @@genres
+  end
+
+  def genre_count 
+    
+  end 
+
+
+end
